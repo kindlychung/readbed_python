1c7f15fabfe10a1ff683bed66183a4d9cd660374
Victor Chung <kindlychung@gmail.com>
2014-07-15 00:37:55 +0200

    reversed order, as required by bed format

---
+++ readbed.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def bitsToNum(x):
+    if x == "00":
+        return 2
+    elif x == "01":
+        return -9
+    elif x == "10":
+        return 1
+    elif x == "11":
+        return 0
+    else:
+        raise Exception("Unknown bits code!")
+
+def byteToNum(xbyte):
+    x = "{:08b}".format(xbyte)
+    xstring = [x[i:i+2] for i in range(0, len(x), 2)]
+    xgen = [str(bitsToNum(i)) for i in xstring]
+    xgen.reverse()
+    xgenstring = """const std::vector<int> """ + "{" + ", ".join(xgen) + "}" + ","
+    return xgenstring
+
+
+# print out c++ code for inclusion in a header file
+print("""const std::vector< const std::vector<int> > gencodes {""")
+for i in range(256):
+    # print("{0:08b}".format(i))
+    print("    ", end="")
+    print(byteToNum(i))
+
+print("}")
